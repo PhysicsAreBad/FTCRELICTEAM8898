0d3c1cbbd47ff043fafc9368d0a71e09e7493a1d
Jack Gonser <jackgonser@gmail.com>
2017-10-02 15:59:52 -1000

    practice

--- FtcRobotController/src/main/java/org/WAFFLES.java
+++ FtcRobotController/src/main/java/org/WAFFLES.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -24,5 +25,29 @@ private DcMotor leftFront = null;
     rightFront = hardwareMap.get(DcMotor.class, "frontRight");
     leftBack = hardwareMap.get(DcMotor.class, "backLeft");
     rightBack = hardwareMap.get(DcMotor.class, "backRight");
+
+    leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+    leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+    rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+    rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    waitForStart();
+
+    while (opModeIsActive()) {
+
+        leftFront.setPower(gamepad1.left_stick_y);
+        leftBack.setPower(gamepad1.left_stick_y);
+        rightBack.setPower(gamepad1.right_stick_y);
+        rightFront.setPower(gamepad1.right_stick_y);
+
+        if(gamepad1.a == true) {
+            leftFront.setPower(-1.0);
+            leftBack.setPower(-1.0);
+            rightBack.setPower(1.0);
+            rightFront.setPower(1.0);
+            telemetry.addData("yay your spining", "");
+            telemetry.update();
+        }
+    }
 }
 }
