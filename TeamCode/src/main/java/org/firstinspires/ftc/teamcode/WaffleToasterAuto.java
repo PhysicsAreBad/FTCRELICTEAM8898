961a51904641502282099abe19025796a668ff16
Jack Gonser <jackgonser@gmail.com>
2017-10-16 16:34:09 -1000

    start auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WaffleToasterAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WaffleToasterAuto.java
@@ -1,10 +1,56 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Created by student on 10/16/2017.
+ * Created by Jack Gonser on 10/16/2017.
  */
-
+@Autonomous (name = "WaffleToasterAuto", group = "Auto")
+//@Disabled
 public class WaffleToasterAuto extends LinearOpMode {
+    private DcMotor leftFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightFront = null;
+    private DcMotor rightBack = null;
+
+    private DcMotor arm = null;
+
+    private Servo leftServo = null;
+    private Servo rightServo = null;
+
+    long millis = System.currentTimeMillis() % 1000;
+    long millisReset;
+    boolean openRight= false;
+    boolean openLeft = false;
+    public void runOpMode ()  throws InterruptedException{\
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        arm = hardwareMap.get(DcMotor.class, "arm");
+
+        leftServo = hardwareMap.get(Servo.class, "leftServo");
+        rightServo = hardwareMap.get(Servo.class, "rightServo");
+
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftServo.setDirection(Servo.Direction.FORWARD);
+        rightServo.setDirection(Servo.Direction.REVERSE);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            
+        }
+    }
 }
