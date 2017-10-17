06adf968ef2bff0df1e31cb2276616c96096c52a
Jack Gonser <jackgonser@gmail.com>
2017-10-16 16:33:19 -1000

    github said so

--- FtcRobotController/src/main/java/org/WAFFLES.java
+++ FtcRobotController/src/main/java/org/WAFFLES.java
@@ -1,51 +1,42 @@
 package org;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by student on 9/25/2017.
  */
 @TeleOp (name="Basic: Waffle Lin OpMode", group="Linear Opmode")
 public class WAFFLES extends LinearOpMode {
-private DcMotor leftFront = null;
-    private DcMotor rightFront = null;
-    private DcMotor leftBack = null;
-    private DcMotor rightBack = null;
 
-@Override
+    @Override
     public void runOpMode(){
-    leftFront = hardwareMap.get(DcMotor.class, "frontLeft");
-    rightFront = hardwareMap.get(DcMotor.class, "frontRight");
-    leftBack = hardwareMap.get(DcMotor.class, "backLeft");
-    rightBack = hardwareMap.get(DcMotor.class, "backRight");
 
-    leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
-    leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
-    rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
-    rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        HardwareWaffles robot = new HardwareWaffles();
+        robot.init(hardwareMap);
     waitForStart();
 
     while (opModeIsActive()) {
 
-        leftFront.setPower(gamepad1.left_stick_y);
-        leftBack.setPower(gamepad1.left_stick_y);
-        rightBack.setPower(gamepad1.right_stick_y);
-        rightFront.setPower(gamepad1.right_stick_y);
+        robot.leftFront.setPower(gamepad1.left_stick_y);
+        robot.leftBack.setPower(gamepad1.left_stick_y);
+        robot.rightBack.setPower(gamepad1.right_stick_y);
+        robot.rightFront.setPower(gamepad1.right_stick_y);
 
         if(gamepad1.a == true) {
-            leftFront.setPower(-1.0);
-            leftBack.setPower(-1.0);
-            rightBack.setPower(1.0);
-            rightFront.setPower(1.0);
-            telemetry.addData("yay your spining", "");
+            robot.leftFront.setPower(-1.0);
+            robot.leftBack.setPower(-1.0);
+            robot.rightBack.setPower(1.0);
+            robot.rightFront.setPower(1.0);
+            telemetry.addData("yay your spinning counter clockwise", "");
+            telemetry.update();
+        }
+        if (gamepad1.b == true) {
+            robot.leftFront.setPower(1.0);
+            robot.leftBack.setPower(1.0);
+            robot.rightBack.setPower(-1.0);
+            robot.rightFront.setPower(-1.0);
+            telemetry.addData("yay your spinning clockwise", "");
             telemetry.update();
         }
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WaffleToasterAuto.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by student on 10/16/2017.
+ */
+
+public class WaffleToasterAuto extends LinearOpMode {
+}
